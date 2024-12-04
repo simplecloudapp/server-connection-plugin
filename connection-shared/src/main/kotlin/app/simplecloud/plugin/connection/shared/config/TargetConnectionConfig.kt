cef7b99db75d54b0c260a1eaa4c4398bb2402441
Kaseax <greenpepper1489@gmail.com>
2024-12-04 18:20:38 +0100

    fix: add @ConfigSerializable to configuration data classes

--- connection-shared/src/main/kotlin/app/simplecloud/plugin/connection/shared/config/TargetConnectionConfig.kt
+++ connection-shared/src/main/kotlin/app/simplecloud/plugin/connection/shared/config/TargetConnectionConfig.kt
@@ -1,5 +1,8 @@
 package app.simplecloud.plugin.connection.shared.config
 
+import org.spongepowered.configurate.objectmapping.ConfigSerializable
+
+@ConfigSerializable
 data class TargetConnectionConfig (
     val name: String = "",
     val priority: Int = 0,
