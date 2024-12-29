362bf6537c9ed3d5157400b6253763eec3043288
InvalidJoker <0limitdev@gmail.com>
2024-12-29 20:36:37 +0100

    fix: invalid data folder

--- connection-waterdog/src/main/kotlin/app/simplecloud/plugin/connection/waterdog/WaterdogServerConnectionPlugin.kt
+++ connection-waterdog/src/main/kotlin/app/simplecloud/plugin/connection/waterdog/WaterdogServerConnectionPlugin.kt
@@ -5,23 +5,24 @@ import app.simplecloud.plugin.connection.shared.server.ServerConnectionInfo
 import dev.waterdog.waterdogpe.player.ProxiedPlayer
 import dev.waterdog.waterdogpe.plugin.Plugin
 
-class WaterdogServerConnectionPlugin: Plugin() {
-
-
-    private val serverConnection = ServerConnectionPlugin<ProxiedPlayer>(
-       dataFolder.toPath(),
-        {
-            proxy.servers.map {
-                ServerConnectionInfo(
-                    it.serverName,
-                    it.players.size
-                )
-            }
-        },
-        { player, permission -> player.hasPermission(permission) }
-    )
-
+class WaterdogServerConnectionPlugin : Plugin() {
+    private lateinit var serverConnection: ServerConnectionPlugin<ProxiedPlayer>
     override fun onEnable() {
+
+
+        serverConnection = ServerConnectionPlugin(
+            dataFolder.toPath(),
+            {
+                proxy.servers.map {
+                    ServerConnectionInfo(
+                        it.serverName,
+                        it.players.size
+                    )
+                }
+            },
+            { player, permission -> player.hasPermission(permission) }
+        )
+
         proxy.reconnectHandler = ReconnectHandler(proxy, serverConnection)
         proxy.joinHandler = JoinHandler(proxy, serverConnection)
     }
