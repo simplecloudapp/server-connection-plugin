0dded6e9b3f4dd22ab9a64928c00516293424452
Till Hoffmann <mail@tillhfm.de>
2025-01-10 19:29:11 +0100

    fix: bug in connection finding process

--- connection-shared/src/main/kotlin/app/simplecloud/plugin/connection/shared/ServerConnectionPlugin.kt
+++ connection-shared/src/main/kotlin/app/simplecloud/plugin/connection/shared/ServerConnectionPlugin.kt
@@ -37,10 +37,10 @@ class ServerConnectionPlugin<P>(
 
     private fun getConnectionAndName(player: P, targetConnections: List<TargetConnectionConfig>, fromServerName: String = ""): Pair<ConnectionAndTargetConfig, String>? {
         val possibleConnections = getPossibleServerConnections(player)
-        val possibleConnectionsWithTarget = possibleConnections.map { possibleConnection ->
+        val possibleConnectionsWithTarget = possibleConnections.mapNotNull { possibleConnection ->
             val targetConfig= targetConnections
                 .filter { fromServerName.isBlank() || matchesTargetConnection(it, fromServerName) }
-                .firstOrNull { possibleConnection.name == it.name } ?: return null
+                .firstOrNull { possibleConnection.name == it.name } ?: return@mapNotNull null
             ConnectionAndTargetConfig(possibleConnection, targetConfig)
         }
 
